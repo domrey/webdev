0d3ec3f494cd61c651d49e239415156c821a6c7e
Jorge Dominguez <jdomrey@gmail.com>
2018-09-24 14:12:14 -0500

    Se añade la referencia al script rh_ausencia_tipo para eliminar, en caso de que exista

--- syslog/sql/db_down.sql
+++ syslog/sql/db_down.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS syslog.rh_movimiento;
 DROP TABLE IF EXISTS syslog.rh_ausencia;
 DROP TABLE IF EXISTS syslog.rh_ausencia_motivo;
+DROP TABLE IF EXISTS syslog.rh_ausencia_tipo;
 DROP TABLE IF EXISTS syslog.rh_ausencia_clase;
 DROP TABLE IF EXISTS syslog.rh_plaza;
 DROP TABLE IF EXISTS syslog.rh_puesto;
